2d92cc29c5e3a61847893ebd71462d230535ec6c
koraykoca <koraykoca34@gmail.com>
2021-12-13 22:32:58 +0100

    local GIS data in GUI

--- main.cpp
+++ main.cpp
@@ -1,62 +1,61 @@
-#include <iostream>
-#include <string>
-
 // Qt includes
-#include <QApplication>  // create QApplication instance
-#include <QPushButton>  // base class of all Qt push buttons
-#include <QWidget>   // base class of all Qt widgets
+#include <QString>
+#include <QApplication>               // create QApplication instance
 
 // QGIS Includes
-#include <qgsapplication.h>
-#include <qgsproviderregistry.h>
-#include <qgssinglesymbolrenderer.h>
-#include <qgsvectorlayer.h>
-#include <qgsrasterlayer.h>
+#include <qgsapplication.h>           // QgsApplication instead of Qt's QApplication to get some added benifits of various static methods that can be used to locate library paths and so on.
+#include <qgsproviderregistry.h>      // The provider registry is a singleton that keeps track of vector data provider plugins. It does all the work for you of loading the plugins and so on.
+#include <qgssinglesymbolrenderer.h>  // most basic symbology class. It renders points, lines or polygons in a single colour. Every vector layer must have a symbology associated with it.
 #include <qgsproject.h>
-#include <qgsmapcanvas.h>
-
-// QGIS Map tools
-#include "qgsmaptoolzoom.h"
-#include "qgsmaptoolpan.h"
+#include <qgsvectorlayer.h>           // The vector layer class inherits from maplayer and extends it to include specialist functionality for vector data.
+#include <qgsmapcanvas.h>             // the nub of the matter. Its the drawable widget that our map will be drawn onto.
+#include <qgssymbol.h>
 
 int main(int argc, char **argv)
 {
+ // Start the Application
  QgsApplication app (argc, argv, true);
 
- QString uri = "crs=EPSG:3857&format&type=xyz&url=https://mt1.google.com/vt/lyrs%3Ds%26x%3D%7Bx%7D%26y%3D%7By%7D%26z%3D%7Bz%7D&zmax=19&zmin=0";
+ // define some variables
+ QString myPluginsDir = "/home/koray/dev/cpp/QGIS/build-master-qtcreator/output/lib/qgis";
+ QString myLayerPath  = "/home/koray/work-unibw/ldbv_bayern/ATKIS_DGM5_Bereich_Gauting_Luftfahrttechnik_Luft_und_Raumfahrttechnik/Vektordaten_ATKIS_UTM32/601_DLM25_clip_n/geb01_f.shp";
+ QString myLayerBaseName = "geb01_f";  // point to the shapefile
+ QString myProviderName = "ogr";       // it tells qgis which data provider to use to load the file
 
- QgsRasterLayer *rastlayer = new QgsRasterLayer(uri, "google maps", "wms");
+ // Instantiate Provider Registry
+ QgsProviderRegistry::instance(myPluginsDir);  // Its a singleton class so we use the static instance call and pass it the provider lib search path. As it initialises it will scan this path for provider libs.
 
- if (rastlayer->isValid())
-    qDebug("Layer is valid");
+ // create a maplayer instance / create the layer object
+ QgsVectorLayer *mypLayer = new QgsVectorLayer(myLayerPath, myLayerBaseName, myProviderName);
+
+ QgsSingleSymbolRenderer *mypRenderer = new QgsSingleSymbolRenderer(QgsSymbol::defaultSymbol(mypLayer->geometryType()));
+
+ QList <QgsMapLayer*> myLayerSet;
+
+ mypLayer->setRenderer(mypRenderer);
+ if (mypLayer->isValid())
+ {
+   qDebug("Layer is valid");
+ }
  else
-    qDebug("Layer is NOT valid");
+ {
+   qDebug("Layer is NOT valid");
+ }
 
- QgsProject::instance()->addMapLayer(rastlayer);
+ QgsProject::instance()->addMapLayer(mypLayer);
+
+ myLayerSet.append(mypLayer);
 
  QgsMapCanvas *mapcanvas = new QgsMapCanvas();
- QList<QgsMapLayer*> layers;
- layers.append(rastlayer);
+ mapcanvas->setExtent(mypLayer->extent());
+ mapcanvas->enableAntiAliasing(true);
+ mapcanvas->setCanvasColor(QColor(255, 255, 255));
+ mapcanvas->freeze(false);
+ mapcanvas->setWindowTitle("GUI MAP");
 
- mapcanvas->setWindowTitle("MAP");
- mapcanvas->setLayers(layers);
- mapcanvas->setExtent(rastlayer->extent());
- mapcanvas->setDestinationCrs(rastlayer->crs());
- mapcanvas->show();
+ mapcanvas->setLayers(myLayerSet);
+ mapcanvas->setVisible(true);
+ mapcanvas->refresh();
+
  return app.exec();
-
- /*
- QFont font ("Courier");
-
- QPushButton button;
- button.setText("\n\nhier ist mein erster Button!");
- button.setWindowTitle(("Hello"));
- button.setToolTip("A tooltip");
- button.setFont(font);
- button.setIcon(QIcon::fromTheme("face-smile"));
- button.show();
-
- return app.exec(); // it tells our QApplication instance to enter the event loop. This loop
-                    // regularly checks if there are any user inputs.
-*/
 }
