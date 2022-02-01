f24ce14576b0c7af958c369d31d1eddaf588b2fa
koraykoca <koray.koca@tum.de>
2022-02-01 15:11:48 +0100

    put marker on vector layer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -120,10 +120,13 @@ void MainWindow::showCoord(QgsPointXY point)
 void MainWindow::selectCoord(QgsPointXY point)
 {
     if (layers.contains(ptrLayer4) == false){
-        point = mTransform.transform(point);
+        QgsPointXY point_trans = point;
+        point_trans = mTransform.transform(point_trans);
+        textBrowser->setText(QString::number(point_trans.y(), 'f', 4) + " " + QString::number(point_trans.x(), 'f', 4));
     }
-
-    textBrowser->setText(QString::number(point.y(), 'f', 4) + " " + QString::number(point.x(), 'f', 4));
+    else{
+        textBrowser->setText(QString::number(point.y(), 'f', 4) + " " + QString::number(point.x(), 'f', 4));
+    }
 
     QLabel *marker = new QLabel(mpMapCanvas);
     marker->setPixmap(QPixmap(":/mapMarker.png"));
